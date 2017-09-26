d2cd83316ed75df5eadf2a6309ca2a6e52d1fe10
uselessylusses <gogleva.a.a@gmail.com>
2017-09-26 11:35:38 +0100

    data table now shows multiple GO terms associated with a gene

--- server_go_enrichment.R
+++ server_go_enrichment.R
@@ -1,20 +1,21 @@
 #server function for GO-enrichment tab
 
-
 output$GOtable <- renderDataTable({ 
     print(class(input$GOgenes))
     parsed_GOgenes <- unlist(strsplit(input$GOgenes, '\n'))
     print(parsed_GOgenes)
+    GOterm = (mapIds(org.At.tair.db,
+                     keys = parsed_GOgenes,
+                     column = "GO",
+                     keytype = "TAIR",
+                     multiVals = "CharacterList"))
+    flatGO <- sapply(GOterm,function(x) paste(unlist(x),collapse="\n"))
     my_table <- data.frame(genes = parsed_GOgenes,
-                           GOterm = mapIds(org.At.tair.db,
-                                           keys = parsed_GOgenes,
-                                           column = "GO",
-                                           keytype = "TAIR",
-                                           multiVals = "first"),
+                           GO = flatGO,
                            genename = mapIds(org.At.tair.db,
-                                             keys = parsed_GOgenes,
-                                             column = "GENENAME",
-                                             keytype = "TAIR",
-                                             multiVals = "first")
+                                      keys = parsed_GOgenes,
+                                      column = "GENENAME",
+                                      keytype = "TAIR",
+                                      multiVals = "first")
                            )
-})
+}, options=list(aLengthMenu=c(10,30,50),iDisplayLength=10, scrollX=TRUE))

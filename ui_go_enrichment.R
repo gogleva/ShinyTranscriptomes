ac98f6017445fb4bc72d3900d8d02691309ed962
uselessylusses <gogleva.a.a@gmail.com>
2017-09-26 08:17:48 +0100

    added drop-down menu for method selection

--- ui_go_enrichment.R
+++ ui_go_enrichment.R
@@ -3,9 +3,12 @@
 tabPanel("GO enrichment",
          sidebarLayout(
              sidebarPanel(
-             radioButtons('method', 'Select an option',
+             radioButtons('genes', 'Select gene list',
              c('Example data' = "example_data",
-               'Upload data' = "upload_data"))),
+               'Upload data' = "upload_data")),
              
+             selectInput("method", "Select method", 
+                         choices=c('method1', 'method2', 'method3'))),
+
              mainPanel(h3(textOutput("Result")))
          ))

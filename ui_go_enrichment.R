1c5e2483c053ec7160a00aff0e28c681c466778c
uselessylusses <gogleva.a.a@gmail.com>
2017-09-26 09:29:30 +0100

    tabset set-up for GO outputs: plots/table

--- ui_go_enrichment.R
+++ ui_go_enrichment.R
@@ -3,22 +3,29 @@
 tabPanel("GO enrichment",
          sidebarLayout(
              sidebarPanel(
-             radioButtons('genes', 'Select gene list',
-             c('Example data' = "example_data",
-               'Upload data' = "upload_data")),
-             
-             textAreaInput(inputId = "genes",
+             textAreaInput(inputId = "GOgenes",
                        label = "Input gene list",
                        placeholder = 'Paste list of gene ids',
                        height = '200px'),
              
-             selectInput("GOterm", "Select GOterm",
+             actionButton(inputId = "GOsubmitButton",
+                          "Submit", icon = icon("flash"), 
+                          width = NULL,
+                          style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+             hr(),
+             
+             selectInput(inputId = "GOterm",
+                         "Select GOterm",
                          choices = c('biological process',
                                      'molecular function',
                                      'cellular localisation')),
              
-             selectInput("method", "Select method", 
+             selectInput(inputId = "GOmethod", "Select method", 
                          choices = c('method1', 'method2', 'method3'))),
 
-             mainPanel(h3(textOutput("Result")))
-         ))
+               mainPanel(
+                 tabsetPanel(
+                     tabPanel("Table"), 
+                     tabPanel("Plot1"), 
+                     tabPanel("Plot2"))
+             )))

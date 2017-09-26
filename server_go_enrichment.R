3718751fdf3bd514878f899882206d09ea9d21c5
uselessylusses <gogleva.a.a@gmail.com>
2017-09-26 18:00:04 +0100

    fisher is working

--- server_go_enrichment.R
+++ server_go_enrichment.R
@@ -61,39 +61,31 @@ output$GOtable <- renderDataTable({
 }, options=list(aLengthMenu=c(10,30,50),iDisplayLength=10, scrollX=TRUE))
 
 
-output$GOresults <- renderDataTable({ 
-    parsed_GOgenes <- unlist(strsplit(input$GOgenes, '\n'))
-    GOterm = (mapIds(org.At.tair.db,
-                     keys = parsed_GOgenes,
-                     column = "GO",
-                     keytype = "TAIR",
-                     multiVals = "CharacterList"))
+output$GOresults <- renderDataTable({
     
-    flatGO <- sapply(GOterm,function(x) paste(unlist(x),collapse="\n"))
-    list2 <- sapply(flatGO, strsplit, '\n')
+    ##GO enrichment isolated
     
-    # show GO terms based on the selected subontology:
+    # this is our gene universe:
+    geneUniverse <- keys(org.At.tair.db)
+    genesOfInterest <- unlist(strsplit(input$GOgenes, '\n'))
+    geneList <- factor(as.integer(geneUniverse %in% genesOfInterest))
+    names(geneList) <- geneUniverse
     
-    if (input$subOntology == 'biological process') {
-        filtered_terms <- sapply(list2, filterOntology, 'BP')
-    } else if ((input$subOntology == 'molecular function')) {
-        filtered_terms <- sapply(list2, filterOntology, 'MF')
-    } else if ((input$subOntology == 'cellular localisation')) {
-        filtered_terms <- sapply(list2, filterOntology, 'CC')
-    }
+    # create GOdata object
     
-    # format list for final display
-    flatGO <- sapply(filtered_terms,function(x) paste(unlist(x),collapse=", "))
+    geneID2GO <- readMappings("data/id2go")
+    myGOdata <- new("topGOdata",
+                    description="My project",
+                    ontology="BP",
+                    allGenes=geneList,
+                    annot = annFUN.gene2GO,
+                    gene2GO = geneID2GO)
+    #
     
-    # dataframe to be displayed in the main panel
-    my_table <- data.frame(gene_ID = parsed_GOgenes,
-                           GO_term = flatGO)
-  
+    resultFisher <- runTest(myGOdata, algorithm="classic", statistic="fisher") 
+    allRes <- GenTable(myGOdata, classicFisher = resultFisher, orderBy = "resultFisher", ranksOf = "classicFisher", topNodes = 10)
+    allRes
 }, options=list(aLengthMenu=c(10,30,50),iDisplayLength=10, scrollX=TRUE))
-
-output$GOgrouped <- renderDataTable({
-    pass
-})
 
 
 output$GOgrouped <- renderDataTable({

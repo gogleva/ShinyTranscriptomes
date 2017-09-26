91aa3cadcdb797f731ca411180b13697072b2eb6
uselessylusses <gogleva.a.a@gmail.com>
2017-09-26 08:05:39 +0100

    dummy sidebar panel for GO-enrichment tab

--- ui_go_enrichment.R
+++ ui_go_enrichment.R
@@ -1,3 +1,11 @@
 # tabPanel for GO-enrichment button:
 
-tabPanel("GO enrichment")
+tabPanel("GO enrichment",
+         sidebarLayout(
+             sidebarPanel(
+             radioButtons('method', 'Select an option',
+             c('Example data' = "example_data",
+               'Upload data' = "upload_data"))),
+             
+             mainPanel(h3(textOutput("Result")))
+         ))

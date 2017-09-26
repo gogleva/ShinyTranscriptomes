7ebc6614d5beb25c7003066f1193c6b8312444ae
uselessylusses <gogleva.a.a@gmail.com>
2017-09-26 14:38:25 +0100

    the main data table now shows only GOterms matching subontology selected

--- server_go_enrichment.R
+++ server_go_enrichment.R
@@ -1,6 +1,7 @@
 #server function for GO-enrichment tab
 
 output$GOtable <- renderDataTable({ 
+ #   print(input$subOntology)
     parsed_GOgenes <- unlist(strsplit(input$GOgenes, '\n'))
     GOterm = (mapIds(org.At.tair.db,
                      keys = parsed_GOgenes,
@@ -8,19 +9,37 @@ output$GOtable <- renderDataTable({
                      keytype = "TAIR",
                      multiVals = "CharacterList"))
     
-    
     flatGO <- sapply(GOterm,function(x) paste(unlist(x),collapse="\n"))
+    list2 <- sapply(flatGO, strsplit, '\n')
     
+    # show GO terms based on the selected subontology:
+
+    if (input$subOntology == 'biological process') {
+        filtered_terms <- sapply(list2, filterOntology, 'BP')
+    } else if ((input$subOntology == 'molecular function')) {
+        filtered_terms <- sapply(list2, filterOntology, 'MF')
+    } else if ((input$subOntology == 'cellular localisation')) {
+        filtered_terms <- sapply(list2, filterOntology, 'CC')
+    }
+    
+    # format list for final display
+    flatGO <- sapply(filtered_terms,function(x) paste(unlist(x),collapse="\n"))
+    
+    # dataframe to be displayed in the main panel
     my_table <- data.frame(gene_ID = parsed_GOgenes,
                            GO_term = flatGO,
                            description = mapIds(org.At.tair.db,
                                       keys = parsed_GOgenes,
                                       column = "GENENAME",
                                       keytype = "TAIR",
-                                      multiVals = "first")
+                                      multiVals = "first"),
+                           symbol = mapIds(org.At.tair.db,
+                                           keys = parsed_GOgenes,
+                                           column = "SYMBOL",
+                                           keytype = "TAIR",
+                                           multiVals = "first")
                            )
 }, options=list(aLengthMenu=c(10,30,50),iDisplayLength=10, scrollX=TRUE))
-
 
 output$GOdownload <- downloadHandler(
     filename = function() {
@@ -31,4 +50,8 @@ output$GOdownload <- downloadHandler(
         write.csv(unlist(strsplit(input$GOgenes, '\n')), con)
     }
 )
+
+
+
+
 
